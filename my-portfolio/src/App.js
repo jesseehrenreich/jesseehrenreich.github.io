0e30aa2f2cf82a560222f7c082b3bbe7713132c9
Jesse Ehrenreich <jesseehrenreich@gmail.com>
2024-09-24 12:33:16 +0200

    Added welcome text

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! My name is Jesse Ehrenreich! This will soon be my portfolio website :)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
